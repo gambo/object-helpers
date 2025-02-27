67ae2f8ae237e02d058fa6f7f5b1f4fc232fa693
Shaun Richardson <srconstruct@gmail.com>
2025-02-27 21:18:18 +1300

    Updated tests and readmes

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # object-helpers
 
+## Why not Lodash.
+You should absolutely use lodash. The constraints for re-creating this deep setter and getter pair of functions was
+to streamline it for use in constructing unknown-type ad-hoc JSON formstate, thus it's implementation has a narrower success range.
+
+This library 
 ## JSON type
 
 The JSON type helps rapidly prototype with nested POJO's and arrays without necessarily knowing the type before hand.
@@ -18,9 +23,24 @@ While JS provides optional chaining, there are DX benefits to having a get funct
 ```js
 import { get } from 'object-helpers'
 
-let a = {a: {b: c: {d: [undefined, {e: 'the value'}]}}}
+let a = {a: {b: {c: {d: [undefined, {e: 'the value'}]}}}}
 get(a, 'a.b.c.d.1.2') // 'the value'
 get(a, 'bad.path.here') // undefined
 get(a, '1.2.3') ?? 'oops' // oops
 get(a, 'a.b.c.d.2') // {e: 'the value'}
 ```
+
+## set function
+
+The set function is a lean implementation of 'lodash.set'. It will set a nested path in dot-notation on an object or array.
+Array indices are anything that can parse to integer. 
+
+All successful invocations will return the object modified in place. All failures will trigger an Exception
+```js
+import { set } from 'object-helpers'
+
+let a = {a: {b: {c: {d: {e: 1}}}}}
+set(a, 'a.b.c.d.1.2', 'new value') // {a: {b: {c: {d: [undefined, [undefined, undefined, 'new value']]}}}}
+set([], '0.0.0.0', 'nested arrays') // [[[['nested arrays']]]]
+```
+
--- src/lib/index.test.ts
+++ src/lib/index.test.ts
@@ -1,55 +1,78 @@
-import { test, expect } from 'vitest';
+import { describe, test, expect } from 'vitest';
 import { get, set } from './index.js';
 
-test('Get object path', async () => {
-	const a = { a: { b: { c: { d: { e: 1 } } } } };
-	expect(get(a, 'a.b.c.d.e')).toBe(1);
-	const b = { a: { b: ['', { c: { d: { e: ['', '', 1] } } }] } };
-	expect(get(b, 'a.b.1.c.d.e.2')).toBe(1);
-});
+describe('Get function tests', () => {
+	test('Get object path', async () => {
+		const a = { a: { b: { c: { d: { e: 1 } } } } };
+		expect(get(a, 'a.b.c.d.e')).toBe(1);
+		const b = { a: { b: ['', { c: { d: { e: ['', '', 1] } } }] } };
+		expect(get(b, 'a.b.1.c.d.e.2')).toBe(1);
+	});
 
-test('Get from Array', async () => {
-	const a = [1, 2, 3, { a: { b: [4, 5, 6, { c: { d: 7 } }] } }, 8, 9];
-	expect(get(a, '3.a.b.3.c.d')).toBe(7);
-	const b = [[[[[[['qwe']]]]]]];
-	expect(get(b, '0.0.0.0.0.0.0')).toBe('qwe');
-});
+	test('Get from Array', async () => {
+		const a = [1, 2, 3, { a: { b: [4, 5, 6, { c: { d: 7 } }] } }, 8, 9];
+		expect(get(a, '3.a.b.3.c.d')).toBe(7);
+		const b = [[[[[[['qwe']]]]]]];
+		expect(get(b, '0.0.0.0.0.0.0')).toBe('qwe');
+	});
 
-test('Get from undefineds and other undesirables', async () => {
-	expect(get([], '3.a.b.3.c.d')).toBe(undefined);
-	expect(get({}, '0.0.0.0.0.0.0')).toBe(undefined);
-	// @ts-expect-error testing bad inputs
-	expect(get(undefined, '0.0.0.0.0.0.0')).toBe(undefined);
-	// @ts-expect-error testing bad inputs
-	expect(get(null, '0.0.0.0.0.0.0')).toBe(undefined);
-	expect(get(NaN, 'a.b.c.d.e')).toBe(undefined);
-	expect(get(123, 'a.b.c.d.e')).toBe(undefined);
-	expect(get('qwe', 'a.b.c.d.e')).toBe(undefined);
-	expect(get(true, 'a.b.c.d.e')).toBe(undefined);
-	expect(get(0, 'a.b.c.d.e')).toBe(undefined);
-	expect(get(1, 'a.b.c.d.e')).toBe(undefined);
-});
-
-test('Set object path', async () => {
-	const a = { a: { b: { c: { d: { e: 1 } } } } };
-	expect(set(a, 'a.b.c.d.e', 2)).toStrictEqual({ a: { b: { c: { d: { e: 2 } } } } });
-});
-test('Set array path', async () => {
-	const a = {};
-	expect(set(a, 'a.1.2.3', 2)).toMatchObject({
-		a: [undefined, [undefined, undefined, [undefined, undefined, undefined, 2]]]
+	test('Get from undefineds and other undesirables', async () => {
+		expect(get([], '3.a.b.3.c.d')).toBe(undefined);
+		expect(get({}, '0.0.0.0.0.0.0')).toBe(undefined);
+		// @ts-expect-error testing bad inputs
+		expect(get(undefined, '0.0.0.0.0.0.0')).toBe(undefined);
+		// @ts-expect-error testing bad inputs
+		expect(get(null, '0.0.0.0.0.0.0')).toBe(undefined);
+		expect(get(NaN, 'a.b.c.d.e')).toBe(undefined);
+		expect(get(123, 'a.b.c.d.e')).toBe(undefined);
+		expect(get('qwe', 'a.b.c.d.e')).toBe(undefined);
+		expect(get(true, 'a.b.c.d.e')).toBe(undefined);
+		expect(get(0, 'a.b.c.d.e')).toBe(undefined);
+		expect(get(1, 'a.b.c.d.e')).toBe(undefined);
 	});
 });
-test('Start with an array', async () => {
-	const a:Array<{name: string}> = [];
-	set(a, '0.name', 'shaun');
-	set(a, '1.name', 'john');
-	set(a, '2.name', 'paul');
-	set(a, '3.name', 'ringo');
-	expect(a).toMatchObject([
-		{ name: 'shaun' },
-		{ name: 'john' },
-		{ name: 'paul' },
-		{ name: 'ringo' }
-	]);
+describe('Set function tests', () => {
+	test('Set object path', async () => {
+		const a = { a: { b: { c: { d: { e: 1 } } } } };
+		expect(set(a, 'a.b.c.d.e', 2)).toStrictEqual({ a: { b: { c: { d: { e: 2 } } } } });
+	});
+	test('Set array path', async () => {
+		const a = {};
+		expect(set(a, 'a.1.2.3', 2)).toMatchObject({
+			a: [undefined, [undefined, undefined, [undefined, undefined, undefined, 2]]]
+		});
+	});
+	test('Start with an array', async () => {
+		const a: Array<{ name: string }> = [];
+		set(a, '0.name', 'shaun');
+		set(a, '1.name', 'john');
+		set(a, '2.name', 'paul');
+		set(a, '3.name', 'ringo');
+		expect(a).toMatchObject([
+			{ name: 'shaun' },
+			{ name: 'john' },
+			{ name: 'paul' },
+			{ name: 'ringo' }
+		]);
+	});
+
+	test('Start with an array', async () => {
+		const a: Array<{ name: string }> = [];
+		set(a, '0.name', 'shaun');
+		set(a, '1.name', 'john');
+		set(a, '2.name', 'paul');
+		set(a, '3.name', 'ringo');
+		expect(a).toMatchObject([
+			{ name: 'shaun' },
+			{ name: 'john' },
+			{ name: 'paul' },
+			{ name: 'ringo' }
+		]);
+	});
+
+	test('Read me examples', () => {
+		const a = {};
+		expect(set(a, 'a.b.c.d.1.2', 'new value')).toMatchObject({a: {b: {c: {d: [undefined, [undefined, undefined, 'new value']]}}}})
+		expect(set([], '0.0.0.0', 'nested arrays')).toMatchObject([[[['nested arrays']]]])
+	});
 });
